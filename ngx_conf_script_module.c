b2232097932c956655379f3dea0a60f292d56074
Guillaume Outters <guillaume-nginx@outters.eu>
2019-11-15 00:55:11 +0100

    conf_scripts command
    + Defines, modifies and resets the config scripts delimiter used by the currently parsed config file and all its includees.
    
    darcs-hash:f656c52915b627685a9b03cb0b3075aefcf4f258

--- ngx_conf_script_module.c
+++ ngx_conf_script_module.c
@@ -8,7 +8,20 @@
 #include <ngx_module.h>
 
 
+char *ngx_conf_scripts(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+char *ngx_conf_script_start(ngx_conf_t *cf, ngx_str_t *open_delim,
+    ngx_str_t *close_delim);
+char *ngx_conf_script_end(ngx_conf_t *cf);
+
+
 static ngx_command_t  ngx_conf_script_commands[] = {
+
+    { ngx_string("conf_scripts"),
+      NGX_ANY_CONF|NGX_CONF_TAKE1|NGX_CONF_TAKE2,
+      ngx_conf_scripts,
+      0,
+      0,
+      NULL },
 
       ngx_null_command
 };
@@ -28,3 +41,81 @@ ngx_module_t  ngx_conf_script_module = {
     NULL,                                  /* exit master */
     NGX_MODULE_V1_PADDING
 };
+
+
+char *
+ngx_conf_scripts(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    char        *rv;
+    ngx_str_t   *args;
+
+    args = cf->args->elts;
+
+    if (cf->args->nelts == 3) {
+        if (!args[1].len || !args[2].len) {
+            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                               "conf_scripts delimiters should be non-empty");
+            return NGX_CONF_ERROR;
+        }
+
+        rv = ngx_conf_script_start(cf, &args[0], &args[1]);
+    } else {
+        if (args[1].len != 3 || ngx_strncmp(args[1].data, "end", 3) != 0) {
+            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                               "conf_scripts should be passed either the "
+                               "opening and closing delimiters (to start a "
+                               "script section), or \"end\".");
+            return NGX_CONF_ERROR;
+        }
+
+        rv = ngx_conf_script_end(cf);
+    }
+
+    return rv;
+}
+
+
+char *
+ngx_conf_script_start(ngx_conf_t *cf, ngx_str_t *open_delim, ngx_str_t *close_delim)
+{
+    char                     *rv;
+    ngx_conf_script_delim_t  *delim;
+
+    if (cf->conf_file->script_delim
+        && cf->conf_file->script_delim->owner != cf->conf_file)
+    {
+        if ((rv = ngx_conf_script_end(cf)) != NGX_CONF_OK) {
+            return rv;
+        }
+    }
+
+    if (!cf->conf_file->script_delim) {
+        delim = ngx_palloc(cf->temp_pool, sizeof(ngx_conf_script_delim_t));
+        if (!delim) {
+            return NGX_CONF_ERROR;
+        }
+        delim->owner = cf->conf_file;
+        cf->conf_file->script_delim = delim;
+    } else {
+        delim = cf->conf_file->script_delim;
+    }
+
+    ngx_memcpy(&delim->open, open_delim, sizeof(ngx_str_t));
+    ngx_memcpy(&delim->close, close_delim, sizeof(ngx_str_t));
+
+    return NGX_CONF_OK;
+}
+
+
+char *
+ngx_conf_script_end(ngx_conf_t *cf)
+{
+    if (!cf->conf_file->script_delim) {
+        return NGX_CONF_OK;
+    }
+
+    ngx_free(cf->conf_file->script_delim);
+    cf->conf_file->script_delim = NULL;
+
+    return NGX_CONF_OK;
+}
