d734329c9c288ade549c3ff78195dba8983eeec2
Guillaume Outters <guillaume-nginx@outters.eu>
2020-02-08 12:26:30 +0100

    typedef ngx_conf_script_vars_t
    + Structure hosting var / vals.
    + ngx_conf_script_vars_t are chained, to allow being stacked then popped.
    
    darcs-hash:444312be4750118cb259f177ed5142cd63650d3d

--- ngx_conf_def.h
+++ ngx_conf_def.h
@@ -19,6 +19,19 @@ typedef struct {
 } ngx_conf_script_delim_t;
 
 
+typedef struct {
+    ngx_str_t name;
+    ngx_str_t val;
+} ngx_conf_script_var_t;
+
+
+typedef struct ngx_conf_script_vars {
+    ngx_array_t vars;
+    ngx_uint_t block_level;
+    struct ngx_conf_script_vars *next;
+} ngx_conf_script_vars_t;
+
+
 int ngx_conf_complex_value(ngx_conf_t *cf, ngx_str_t *string);
 
 
